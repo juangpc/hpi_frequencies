188697eeabb9bd611c4a6c21ed1e2709fdfe6e64
JuanGPC <juangpc@users.noreply.github.com>
2023-01-10 23:20:05 -0500

    parse input arguments

---
+++ test.sh
@@ -0,0 +1,20 @@
+(
+echo "PASS homunculus122" 
+sleep 0.1
+echo "name $name"
+sleep 0.1
+echo "vara hpiFreq1 = $hpiFreq1"
+sleep 0.1
+echo "vara hpiFreq2 = $hpiFreq2"
+sleep 0.1
+echo "vara hpiFreq3 = $hpiFreq3"
+sleep 0.1
+echo "vara hpiFreq4 = $hpiFreq4"
+sleep 0.1
+echo "vara hpiFreq5 = $hpiFreq5"
+sleep 0.1
+echo "QUIT"
+sleep 0.1
+) | 
+
+echo fin
