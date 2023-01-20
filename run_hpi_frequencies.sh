b3a64226ab8d9e7f67dad416268093afbf81fd29
JuanGPC <juangpc@users.noreply.github.com>
2023-01-20 15:11:18 -0500

    add utils for a desktop shortcut interface

---
+++ run_hpi_frequencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Run the hpi_frequencies command to 
+# change the HPI frequencies according to sampling frequency setting
+#
+
+exec /data/MNE-special/hpi_frequencies >> ~/output.txt
+
+
